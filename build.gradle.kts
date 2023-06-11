c352bd202d6f151f8c581c89132e3a38359ab802
Phillip Schichtel <phillip@schich.tel>
2023-06-11 23:36:10 +0200

    initial poc

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("multiplatform") version "1.8.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                val ktorVersion = "2.3.1"
+                implementation("io.ktor:ktor-network:$ktorVersion")
+                val coroutinesVersion = "1.7.1"
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutinesVersion")
+            }
+        }
+    }
+}
---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,311 @@
+import io.ktor.network.selector.SelectorManager
+import io.ktor.network.sockets.*
+import io.ktor.util.collections.ConcurrentSet
+import io.ktor.utils.io.*
+import io.ktor.utils.io.errors.*
+import kotlinx.cinterop.*
+import kotlinx.coroutines.*
+import platform.posix.*
+import kotlin.system.exitProcess
+
+private fun error(message: String): Nothing {
+    val code = errno
+    throw RuntimeException("$message: ${strerror(code)?.toKString()} ($code)")
+}
+
+private fun errorln(message: String) {
+    if (fprintf(stderr, "$message\n") == -1) {
+        error("Failed to fprintf to stderr")
+    }
+}
+
+data class Pipe(val readEnd: Int, val writeEnd: Int) {
+    private fun closeFd(fd: Int) {
+        val status = close(fd)
+        if (status != 0) {
+            error("Failed to close fd $fd")
+        }
+    }
+
+    fun closeRead() {
+        closeFd(readEnd)
+    }
+
+    fun closeWrite() {
+        closeFd(writeEnd)
+    }
+
+    fun close() {
+        closeRead()
+        closeWrite()
+    }
+}
+
+private fun mkpipe(): Pipe {
+    val fds = IntArray(2)
+    val status = fds.usePinned {
+        pipe(it.addressOf(0))
+    }
+    if (status != 0) {
+        error("Failed to create pipe")
+    }
+    return Pipe(fds[0], fds[1])
+}
+
+private fun redirect(old: Int, new: Int) {
+    if (dup2(old, new) == -1) {
+        error("Failed to dup2 $old to $new")
+    }
+    if (close(old) == -1) {
+        error("Failed to close old fd $old")
+    }
+}
+
+enum class StdStream(val fd: Int) {
+    In(STDIN_FILENO),
+    Out(STDOUT_FILENO),
+    Err(STDERR_FILENO),
+}
+
+private fun redirectPipe(pipe: Pipe, stream: StdStream) {
+    when (stream) {
+        StdStream.In -> {
+            pipe.closeWrite()
+            redirect(pipe.readEnd, stream.fd)
+        }
+        StdStream.Out, StdStream.Err -> {
+            pipe.closeRead()
+            redirect(pipe.writeEnd, stream.fd)
+        }
+    }
+}
+
+data class Process(val pid: Int, val stdin: ByteWriteChannel, val stdout: ByteReadChannel, val stderr: ByteReadChannel, val exitCode: Deferred<Int>)
+
+private fun writeFully(fd: Int, buffer: Pinned<ByteArray>, offset: Int, length: Int): Int {
+    var bytesWritten = 0
+    while (bytesWritten < length) {
+        val newBytesWritten = write(
+            fd,
+            buffer.addressOf(offset + bytesWritten),
+            (length - bytesWritten).convert()
+        ).toInt()
+        if (newBytesWritten == -1) {
+            break
+        }
+        bytesWritten += newBytesWritten
+    }
+    return bytesWritten
+}
+
+private fun printBuffer(buffer: Pinned<ByteArray>, offset: Int, length: Int): Int {
+    return writeFully(STDOUT_FILENO, buffer, offset, length)
+}
+
+private fun CoroutineScope.subProcess(cmdLine: Array<String>): Process {
+    val stdinPipe = mkpipe()
+    val stdoutPipe = mkpipe()
+    val stderrPipe = mkpipe()
+
+    val childPid = fork()
+    if (childPid == -1) {
+        error("Failed to fork!")
+    }
+    if (childPid == 0) {
+        execSubprocess(cmdLine, stdinPipe, stdoutPipe, stderrPipe)
+    } else {
+        val deferredExitCode = CompletableDeferred<Int>()
+
+        stdinPipe.closeRead()
+        stdoutPipe.closeWrite()
+        stderrPipe.closeWrite()
+
+        fun CoroutineScope.forwardOutput(pipe: Pipe, channel: ByteWriteChannel) {
+            launch(Dispatchers.IO) {
+                val buffer = ByteArray(8192)
+                buffer.usePinned {
+                    while (isActive) {
+                        val bytesRead = read(pipe.readEnd, it.addressOf(0), buffer.size.convert())
+                        if (bytesRead == -1L || channel.isClosedForWrite) {
+                            break
+                        }
+                        channel.writeFully(buffer, 0, bytesRead.toInt())
+                        channel.flush()
+                    }
+                }
+                errorln("Output forwarder stopped")
+            }
+        }
+
+        fun CoroutineScope.forwardInput(channel: ByteReadChannel, pipe: Pipe) {
+            launch(Dispatchers.IO) {
+                ByteArray(8192).usePinned { pinnedBuffer ->
+                    val actualBuffer = pinnedBuffer.get()
+                    while (isActive) {
+                        val bytesRead = channel.readAvailable(actualBuffer, 0, actualBuffer.size)
+                        if (bytesRead == -1) {
+                            errorln("input channel closed")
+                            break
+                        }
+                        errorln("received data for child: ${actualBuffer.decodeToString(0, bytesRead)}")
+                        val bytesWritten = writeFully(pipe.writeEnd, pinnedBuffer, 0, bytesRead)
+                        if (bytesWritten != bytesRead) {
+                            errorln("Not all bytes have been written, expected $bytesRead, but wrote $bytesWritten")
+                            break
+                        }
+                    }
+                }
+                errorln("Input forwarder stopped")
+            }
+        }
+
+        val stdinChannel = ByteChannel()
+        val stdoutChannel = ByteChannel()
+        val stderrChannel = ByteChannel()
+
+        forwardInput(stdinChannel, stdinPipe)
+        forwardOutput(stdoutPipe, stdoutChannel)
+        forwardOutput(stderrPipe, stderrChannel)
+
+        launch(Dispatchers.IO) {
+            val exitCode = memScoped {
+                val status = alloc<Int>(0)
+                waitpid(childPid, status.ptr, 0)
+                (status.value and 0xFF00) shr 8
+            }
+
+            deferredExitCode.complete(exitCode)
+
+            stdinChannel.close()
+            stdinPipe.close()
+            stdoutChannel.close()
+            stdoutPipe.close()
+            stderrChannel.close()
+            stderrPipe.close()
+        }
+
+        return Process(childPid, stdinChannel, stdoutChannel, stderrChannel, deferredExitCode)
+    }
+}
+
+private fun execSubprocess(cmdLine: Array<String>, stdin: Pipe, stdout: Pipe, stderr: Pipe): Nothing {
+    redirectPipe(stdin, StdStream.In)
+    redirectPipe(stdout, StdStream.Out)
+    redirectPipe(stderr, StdStream.Err)
+
+    val status = memScoped {
+        val argv = allocArray<CPointerVar<ByteVar>>(cmdLine.size + 1)
+        for (i in cmdLine.indices) {
+            argv[i] = cmdLine[i].cstr.ptr
+        }
+        argv[cmdLine.size] = null
+        execv(cmdLine[0], argv)
+    }
+
+    if (status != 0) {
+        error("Failed to execv ${cmdLine.joinToString(" ")}")
+    }
+    exitProcess(status)
+}
+
+data class Client(val socket: Socket, val outputChannel: ByteWriteChannel)
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        errorln("Command is missing!")
+        exitProcess(1)
+    }
+    val clients = ConcurrentSet<Client>()
+
+    runBlocking {
+        val childProcess = subProcess(args)
+
+        val selectorManager = SelectorManager(Dispatchers.IO)
+        val serverSocket = aSocket(selectorManager).tcp().bind("0.0.0.0", 8080) {
+            reuseAddress = true
+        }
+
+        launch(Dispatchers.Default) {
+            val exitCode = childProcess.exitCode.await()
+            errorln("Child process `${args.joinToString(" ")}` exited: $exitCode")
+            selectorManager.cancel()
+        }
+
+        launch(Dispatchers.IO) {
+            ByteArray(8192).usePinned { pinnedBuffer ->
+                val buffer = pinnedBuffer.get()
+                while (isActive) {
+                    val bytesRead = childProcess.stdout.readAvailable(buffer, 0, buffer.size)
+                    if (bytesRead == -1) {
+                        break
+                    }
+                    if (bytesRead > 0) {
+                        printBuffer(pinnedBuffer, 0, bytesRead)
+
+                        if (clients.isNotEmpty()) {
+                            coroutineScope {
+                                for (client in clients) {
+                                    launch(Dispatchers.IO) {
+                                        client.outputChannel.writeFully(buffer, 0, bytesRead)
+                                        client.outputChannel.flush()
+                                        errorln("broadcasted to ${client.socket.remoteAddress}")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            errorln("stdout broadcaster: ended")
+        }
+
+        launch(Dispatchers.IO) {
+            while (isActive && !serverSocket.isClosed) {
+                errorln("Awaiting socket...")
+                val socket = try {
+                    serverSocket.accept()
+                } catch (e: IOException) {
+                    errorln(e.message ?: "Unknown error during accept")
+                    break
+                }
+                errorln("Client connected: ${socket.remoteAddress}")
+
+                socket.launch {
+                    val readChannel = socket.openReadChannel()
+                    val writeChannel = socket.openWriteChannel(autoFlush = false)
+                    val client = Client(socket, writeChannel)
+                    clients.add(client)
+
+                    ByteArray(8192).usePinned { pinnedBuffer ->
+                        val buffer = pinnedBuffer.get()
+                        while (isActive) {
+                            readChannel.awaitContent()
+                            if (readChannel.isClosedForRead) {
+                                errorln("Client disconnected: ${socket.remoteAddress}")
+                                clients.remove(client)
+                                break
+                            }
+                            val bytesRead = readChannel.readAvailable(buffer)
+                            if (bytesRead > 0) {
+                                childProcess.stdin.writeFully(buffer, 0, bytesRead)
+                                childProcess.stdin.flush()
+
+                                printBuffer(pinnedBuffer, 0, bytesRead)
+
+                                val otherClients = clients.filter { it != client }
+                                if (otherClients.isNotEmpty()) {
+                                    otherClients.map { otherClient ->
+                                        otherClient.socket.async {
+                                            otherClient.outputChannel.writeFully(buffer, 0, bytesRead)
+                                            otherClient.outputChannel.flush()
+                                        }
+                                    }.awaitAll()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
